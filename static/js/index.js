65766a6a5b11f2b044dfd1a82f5c9a17769f44ad
BOM-98 <64743302+BOM-98@users.noreply.github.com>
2024-01-03 20:55:44 +0000

    feat: responsive nav - base.html

---
+++ static/js/index.js
@@ -0,0 +1,12 @@
+let menuToggle = document.getElementById("menu-toggle");
+const navList = document.getElementById("nav-list");
+
+if (menuToggle && navList) {
+    menuToggle.addEventListener("click", onToggleMenu);
+} else {
+    console.error("Menu toggle or nav list element not found");
+}
+
+function onToggleMenu(e) {
+    navList.classList.toggle("top-[100%]");
+}
