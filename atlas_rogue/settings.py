8dbf359c5cdf9dc1bec81d8b62ae67a1c874a75c
BOM-98 <64743302+BOM-98@users.noreply.github.com>
2024-01-05 17:42:57 +0000

    fix: correct allowed host

--- atlas_rogue/settings.py
+++ atlas_rogue/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://atlas-rogue-09e759974ffc.herokuapp.com/', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['https://atlas-rogue-09e759974ffc.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
