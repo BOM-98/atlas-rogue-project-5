33c8965b278e165bb199a37909d4e6f99758329c
BOM-98 <64743302+BOM-98@users.noreply.github.com>
2024-01-06 16:34:40 +0000

    feat: single dropdown active - base.html

--- static/js/index.js
+++ static/js/index.js
@@ -1,20 +1,52 @@
+/**
+ * Initializes the dropdown functionality once the DOM content is fully loaded.
+ * It sets up event listeners for the menu toggle and dropdown buttons.
+ */
 document.addEventListener("DOMContentLoaded", function () {
   let menuToggle = document.getElementById("menu-toggle");
   const navList = document.getElementById("nav-list");
-  const rentDropdown = document.getElementById("rent-menu-button");
-  const rentDropdownMenu = document.getElementById("rent-dropdown-menu");
+  const dropdownButtons = document.querySelectorAll(".drop-down-button");
+  const dropdownMenus = document.querySelectorAll(".drop-down-menu");
 
+  // Add event listener for nav mobile-menu toggle
   if (menuToggle && navList) {
     menuToggle.addEventListener("click", () => onToggleMenu(navList));
   } else {
     console.error("Menu toggle or nav list element not found");
   }
 
-  if (rentDropdown && rentDropdownMenu) {
-    rentDropdown.addEventListener("click", () => onToggleMenu(rentDropdownMenu));
-  }
-
+  /**
+   * Toggles the visibility of a specified element by adding or removing the 'hidden' class.
+   * @param {HTMLElement} element - The element to toggle visibility for.
+   */  
   function onToggleMenu(element) {
-    element.classList.toggle("hidden");
-  }
+      element.classList.toggle("hidden");
+      console.log("clicked")
+    }
+  
+  // Setup event listeners for each dropdown button  
+  dropdownButtons.forEach(button => {
+      button.addEventListener('click', function(event) {
+        // Close all dropdown menus excluding active one selected
+        dropdownMenus.forEach(menu => {
+          if (menu !== button.nextElementSibling) {
+            menu.classList.add('hidden');
+          }
+        });
+
+        // Toggle the current dropdown menu
+        const dropdownMenu = button.nextElementSibling;
+        dropdownMenu.classList.toggle('hidden');
+
+        // Prevent the click event from propagating to the window
+        event.stopPropagation();
+      });
+    });
+
+    // Close all dropdowns when clicking outside
+    window.addEventListener('click', function() {
+      dropdownMenus.forEach(menu => {
+        menu.classList.add('hidden');
+      });
+    });
 });
