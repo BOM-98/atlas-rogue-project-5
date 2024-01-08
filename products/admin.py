67b106138678ac6731c045402d97702ea90839b2
BOM-98 <bromahon@tcd.ie>
2024-01-08 17:30:06 +0000

    feat: fix models to fit product descriptions

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
 from .models import Product, Category
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "sku",
+        "name",
+        "designer",
+        "price",
+        "rrp",
+        "size",
+        "colour",
+        "length",
+        "image_url",
+        "image",
+    )
+    ordering = ("name",)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "friendly_name",
+        "name",
+    )
+
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import Product, Category
 
 # Create your views here.
+def all_products(request):
+    """
+    Render the products view.
+
+    This view returns the homepage template for
+    the website. It is a simple view that
+    only renders the template without any
+    additional context or processing.
+
+    Parameters:
+    request (HttpRequest): The HttpRequest
+    object that represents the client's request.
+
+    Returns:
+    HttpResponse: An HttpResponse object
+    that renders the 'products/products.html' template.
+    """
+    products = Product.objects.all()
+    context = {
+        "products": products,
+    }
+    return render(request, "products/products.html", context)
