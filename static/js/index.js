e536d8d378796b1bc47c028bfd83de22ad3914b0
BOM-98 <bromahon@tcd.ie>
2024-01-10 20:13:16 +0000

    feat: add featured product section - index.html

--- static/js/index.js
+++ static/js/index.js
@@ -18,35 +18,36 @@ document.addEventListener("DOMContentLoaded", function () {
   /**
    * Toggles the visibility of a specified element by adding or removing the 'hidden' class.
    * @param {HTMLElement} element - The element to toggle visibility for.
-   */  
+   */
   function onToggleMenu(element) {
-      element.classList.toggle("hidden");
-      console.log("clicked")
-    }
-  
-  // Setup event listeners for each dropdown button  
-  dropdownButtons.forEach(button => {
-      button.addEventListener('click', function(event) {
-        // Close all dropdown menus excluding active one selected
-        dropdownMenus.forEach(menu => {
-          if (menu !== button.nextElementSibling) {
-            menu.classList.add('hidden');
-          }
-        });
+    element.classList.toggle("hidden");
+    console.log("clicked");
+  }
 
-        // Toggle the current dropdown menu
-        const dropdownMenu = button.nextElementSibling;
-        dropdownMenu.classList.toggle('hidden');
-
-        // Prevent the click event from propagating to the window
-        event.stopPropagation();
+  // Setup event listeners for each dropdown button
+  dropdownButtons.forEach((button) => {
+    button.addEventListener("click", function (event) {
+      // Close all dropdown menus excluding active one selected
+      dropdownMenus.forEach((menu) => {
+        if (menu !== button.nextElementSibling) {
+          menu.classList.add("hidden");
+        }
       });
-    });
 
-    // Close all dropdowns when clicking outside
-    window.addEventListener('click', function() {
-      dropdownMenus.forEach(menu => {
-        menu.classList.add('hidden');
-      });
+      // Toggle the current dropdown menu
+      const dropdownMenu = button.nextElementSibling;
+      dropdownMenu.classList.toggle("hidden");
+
+      // Prevent the click event from propagating to the window
+      event.stopPropagation();
     });
+  });
+
+  // Close all dropdowns when clicking outside
+  window.addEventListener("click", function () {
+    dropdownMenus.forEach((menu) => {
+      menu.classList.add("hidden");
+    });
+  });
 });
+
