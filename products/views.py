b74ae9a8de51c77ba6e10a7b689115221cf81bcd
BOM-98 <bromahon@tcd.ie>
2024-01-28 20:59:46 +0000

    feat: add category and designer nav links - base.html

--- products/views.py
+++ products/views.py
@@ -10,8 +10,21 @@ def all_products(request):
     """
     products = Product.objects.all()
     query = None
+    category = None
+    designers = None
+    categories = None
     
     if request.GET:
+        if 'category' in request.GET:
+            category = request.GET['category']
+            products = products.filter(category__name__iexact=category)
+            categories = Category.objects.filter(name__iexact=category)
+            
+        if 'designer' in request.GET:
+            designer = request.GET['designer']
+            products = products.filter(designer__iexact=designer)
+            designers = Product.objects.filter(designer__iexact=designer)
+        
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -21,11 +34,13 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(category__name__icontains=query) | Q(designer__icontains=query) | Q(size__icontains=query) | Q(colour__icontains=query) | Q(length__icontains=query)
             products = products.filter(queries)
             
-        context = {
-            "products": products,
-            'search_term': query,
-        }
-        return render(request, "products/products.html", context)
+    context = {
+        "products": products,
+        'search_term': query,
+        'current_categories': categories,
+        'current_designers': designers,
+    }
+    return render(request, "products/products.html", context)
 
 def product_detail(request, product_id):
     """
