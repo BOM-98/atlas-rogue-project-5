57c36896177247de19d4e9318cf440243e70307b
BOM-98 <bromahon@tcd.ie>
2024-01-28 16:12:41 +0000

    fix: correct image sizes

--- products/views.py
+++ products/views.py
@@ -1,26 +1,25 @@
 from django.shortcuts import render
 from .models import Product, Category
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def all_products(request):
     """
-    Render the products view.
-
-    This view returns the homepage template for
-    the website. It is a simple view that
-    only renders the template without any
-    additional context or processing.
-
-    Parameters:
-    request (HttpRequest): The HttpRequest
-    object that represents the client's request.
-
-    Returns:
-    HttpResponse: An HttpResponse object
-    that renders the 'products/products.html' template.
+    Render the products view.    
     """
     products = Product.objects.all()
     context = {
         "products": products,
     }
     return render(request, "products/products.html", context)
+
+def product_detail(request, product_id):
+    """
+    Render the products detail view.
+    """
+    product = get_object_or_404(Product, pk=product_id)
+    
+    context = {
+        "product": product,
+    }
+    return render(request, "products/product_detail.html", context)
