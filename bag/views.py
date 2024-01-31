d0895bdf215502051e7270bd2e90ff6ea3b1ebf1
BOM-98 <bromahon@tcd.ie>
2024-01-31 18:21:30 +0000

    feat: add toasts - base.html

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
+from products.models import Product
+from django.contrib import messages
 
 # Create your views here.
 def view_bag(request):
@@ -17,6 +19,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
     
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get("quantity"))
     redirect_url = request.POST.get("redirect_url")
     bag = request.session.get("bag", {})
@@ -26,6 +29,7 @@ def add_to_bag(request, item_id):
     else:
         bag[item_id] = quantity
     bag[item_id] = quantity
+    messages.success(request, f"Added {product.name} to your bag")
     
     request.session["bag"] = bag
     return redirect(redirect_url)
