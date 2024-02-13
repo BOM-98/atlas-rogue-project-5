03d00eccf115174304aeaa5ef14ecbf827f8e95e
BOM-98 <bromahon@tcd.ie>
2024-02-13 21:53:32 +0000

    feat: add stripe card checkout - checkout.html

--- checkout/views.py
+++ checkout/views.py
@@ -15,5 +15,7 @@ def checkout(request):
     template = "checkout/checkout.html"
     context = {
         "order_form": order_form,
+        "stripe_public_key": "",
+        "client_secret": "",
     }
     return render(request, template, context)
