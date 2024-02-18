2a3d3a2ce980dad90c90271d88113a68740a4155
BOM-98 <bromahon@tcd.ie>
2024-02-18 12:12:28 +0000

    feat: add 404 page - 404.html

--- atlas_rogue/urls.py
+++ atlas_rogue/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from home.views import *
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -31,3 +32,4 @@ urlpatterns = [
     path('wishlist/', include('wishlist.urls')),
     path('contact/', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+handler404 = 'atlas_rogue.views.handler404'
