90ba4ebf7c306edab115a12131ec26f3f681737d
BOM-98 <bromahon@tcd.ie>
2024-02-19 22:43:29 +0000

    feat: add featured section - index.html

--- home/views.py
+++ home/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.db import models
+from products.models import Product, Category
+from wishlist.models import Wishlist
+from profiles.models import UserProfile
 
 # Create your views here.
 def index(request):
@@ -18,5 +22,24 @@ def index(request):
     HttpResponse: An HttpResponse object
     that renders the 'home/index.html' template.
     """
-
-    return render(request, "home/index.html")
+    featured_product_1 = Product.objects.get(pk=49)
+    featured_product_2 = Product.objects.get(pk=63)
+    featured_product_3 = Product.objects.get(pk=66)
+    featured_product_4 = Product.objects.get(pk=76)
+    featured_products = [featured_product_1, featured_product_2, featured_product_3, featured_product_4]
+    categories = Category.objects.all()
+    wishlist = None
+    
+    # Only attempt to get the user's profile and wishlist if the user is authenticated
+    if request.user.is_authenticated:
+        user_profile = UserProfile.objects.get(user=request.user)
+        wishlist, created = Wishlist.objects.get_or_create(user=user_profile)
+    else:
+        wishlist = None  # Wishlist remains None if the user is not authenticated
+        
+    context = {
+        "featured_products": featured_products,
+        "current_categories": categories,
+        "wishlist": wishlist,
+    }
+    return render(request, "home/index.html", context)
