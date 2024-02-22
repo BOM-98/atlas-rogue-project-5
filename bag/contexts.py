3ce3ef0b667ae5784f656e4b9ae515cebca7327e
BOM-98 <bromahon@tcd.ie>
2024-02-22 00:27:00 +0000

    Refactor: bag app code and add docstrings

--- bag/contexts.py
+++ bag/contexts.py
@@ -4,15 +4,25 @@ from django.shortcuts import get_object_or_404
 from products.models import Product
 from datetime import datetime
 
+
 def bag_contents(request):
     """
     A view to return the bag contents page.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        dict: A dictionary containing the bag items, total price,
+        product count, delivery cost,
+        free delivery threshold, and grand total.
+
     """
     bag_items = []
     total = 0
     product_count = 0
     bag = request.session.get("bag", {})
-    
+
     for item_id, item_data in bag.items():
         product = get_object_or_404(Product, pk=item_id)
         start_date = item_data['start_date']
@@ -28,16 +38,16 @@ def bag_contents(request):
             "start_date": start_date,
             "end_date": end_date,
         })
-    
+
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_delta = 0
-        
+
     grand_total = delivery + total
-    
+
     context = {
         "bag_items": bag_items,
         "total": total,
--- bag/views.py
+++ bag/views.py
@@ -14,12 +14,22 @@ def view_bag(request):
     only renders the template without any
     additional context or processing.
     """
-
     return render(request, "bag/bag.html")
 
 def add_to_bag(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag """
-    
+    """
+    Add a quantity of the specified product to the shopping bag.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+        item_id (int): The ID of the product to add to the bag.
+
+    Returns:
+        HttpResponseRedirect: A redirect response to the specified URL.
+
+    Raises:
+        None
+    """
     product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get("quantity"))
     start_date_str = request.POST.get('start_date')
@@ -28,38 +38,48 @@ def add_to_bag(request, item_id):
     end_date = datetime.strptime(end_date_str, '%Y-%m-%d').date()
     redirect_url = request.POST.get("redirect_url")
     bag = request.session.get("bag", {})
-    
+
     # Calculating the difference
     difference = end_date - start_date
 
     # Getting the number of days
     number_of_days = difference.days
     quantity = number_of_days
-    
+
     # Validate that both start_date and end_date are provided
     if not start_date or not end_date:
         messages.error(request, "You must select both a start and an end date.")
         return redirect('product_detail', product_id=item_id)
-    
+
     # Convert date objects to string in ISO format before storing in the session
     if start_date:
         start_date = start_date.isoformat()  # Converts to 'YYYY-MM-DD' string
     if end_date:
         end_date = end_date.isoformat()
-    
+
     if item_id in list(bag.keys()):
         bag[item_id] = {'quantity': quantity, 'start_date': start_date, 'end_date': end_date}
     else:
         bag[item_id] = {'quantity': quantity, 'start_date': start_date, 'end_date': end_date}
     bag[item_id] = {'quantity': quantity, 'start_date': start_date, 'end_date': end_date}
     messages.success(request, f"Added {product.name} to your bag")
-    
+
     request.session["bag"] = bag
     return redirect(redirect_url)
 
 def remove_from_bag(request, item_id):
-    """Remove the item from the shopping bag"""
+    """Remove the item from the shopping bag.
 
+    Args:
+        request (HttpRequest): The HTTP request object.
+        item_id (int): The ID of the item to be removed from the bag.
+
+    Returns:
+        HttpResponseRedirect: Redirects to the 'view_bag' view.
+
+    Raises:
+        HttpResponse: If there is an error removing the item.
+    """
     try:
         product = Product.objects.get(pk=item_id)
         bag = request.session.get('bag', {})
