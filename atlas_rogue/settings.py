3dfdc5d93be8e6f814b751f24769363581083b17
BOM-98 <bromahon@tcd.ie>
2024-02-22 03:00:49 +0000

    chore: set debug to false

--- atlas_rogue/settings.py
+++ atlas_rogue/settings.py
@@ -29,7 +29,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'atlas-rogue-09e759974ffc.herokuapp.com', 'localhost', '127.0.0.1']
