e45c5cd5c00f8147c23f70c645ec131567d37978
BOM-98 <bromahon@tcd.ie>
2024-02-22 00:28:31 +0000

    Refactor: contact module and add docstrings

--- contact/forms.py
+++ contact/forms.py
@@ -1,7 +1,18 @@
 from django import forms
 from .models import Contact
 
+
 class ContactForm(forms.ModelForm):
+    """
+    A form for creating a contact message.
+
+    Attributes:
+        name (str): The name of the user.
+        email (str): The email address of the user.
+        subject (str): The subject of the contact message.
+        message (str): The content of the contact message.
+    """
+
     class Meta:
         model = Contact
         fields = ['name', 'email', 'subject', 'message']
--- contact/views.py
+++ contact/views.py
@@ -4,7 +4,18 @@ from django.contrib import messages
 from .models import Contact
 from django.http import HttpResponseForbidden
 
+
 def contact(request):
+    """
+    Renders the contact form and handles form submission.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The HTTP response object.
+
+    """
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
@@ -16,9 +27,23 @@ def contact(request):
     template = 'contact/contact.html'
     return render(request, template, {'form': form})
 
+
 def contact_submissions(request):
+    """
+    View function that handles the display of contact form submissions.
+
+    Only superusers are allowed to access this page.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The HTTP response object
+        containing the rendered template.
+    """
     if not request.user.is_superuser:
-        messages.error(request, 'Sorry, you do not have permission to view this page.')
+        messages.error(
+            request, 'Sorry, you do not have permission to view this page.')
         return redirect(reverse("home"))
     contacts = Contact.objects.all()
     template = 'contact/contact_form_submissions.html'
